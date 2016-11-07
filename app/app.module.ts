ad75242a5c4818ffe4f6786f881f44d6614b8bc0
Joseph <joseph.lama@hotmail.com>
2016-11-07 17:36:22 +0100

    second commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {HomeComponent} from './components/home/home.component';
+import {ProfileComponent} from './components/profile/profile.component';
+
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent, ProfileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
